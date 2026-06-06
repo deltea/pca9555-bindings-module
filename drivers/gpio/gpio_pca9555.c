4716dc0685499b9c0b992149d2c4fb1ef736730b
deltea <thcheetah777@gmail.com>
2026-06-06 15:49:49 -0700

    rename shi

--- drivers/gpio/gpio_pca9555.c
+++ drivers/gpio/gpio_pca9555.c
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT nxp_pca95xx
+#define DT_DRV_COMPAT nxp_pca9555
 
 /**
  * @file Driver for PCA95XX and PCAL95XX I2C-based GPIO driver.
